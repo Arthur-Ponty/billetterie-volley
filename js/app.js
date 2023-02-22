c18fac8ee7c5e89c2af183624065e351b7c0f896
Arthur <arthur@agence-alchimy.fr>
2023-02-22 11:09:13 +0100

    add log

--- js/app.js
+++ js/app.js
@@ -88,6 +88,7 @@ window.addEventListener("load", () => {
     function prepareEndpoint(result_qr) {
         let endpoint = "";
 
+        console.log(typeof result_qr);
         if(typeof result_qr != 'string' || !result_qr.includes("saint-die-volley.eu")) {
             return "error";
         }
